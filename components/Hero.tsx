1fdceb888f017a244871d4adea9acb3bf02f4c9e
EasyTrip24 <easytrip26@gmail.com>
2026-02-07 15:44:09 +0100

    update prjects images page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -104,19 +104,6 @@ export default function Hero() {
                 </svg>
               </a>
 
-              {/* X (Twitter) (taille réduite) */}
-              <a 
-                href="https://twitter.com" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="w-6 h-6 flex items-center justify-center text-white/70 hover:text-white transition-colors"
-                aria-label="Twitter"
-              >
-                <svg className="w-3.5 h-3.5" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                </svg>
-              </a>
-
               <div className="w-px h-4 bg-white/20" />
 
               {/* Language Switch - réduit */}
